a48935182d537550bba9550db5c0ed943a77b976
Luis Angel Valencia Ramos <luvalencia@sistecredito.com>
2017-09-12 12:26:37 -0500

    Inicio

---
+++ PersonasAppConceptos/PersonasAppConceptos.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonasAppConceptos.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new PersonasAppConceptos.App());
+        }
+    }
+}
---
+++ PersonasAppConceptos/PersonasAppConceptos/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using PersonasAppConceptos.Helpers;
+using PersonasAppConceptos.Models;
+using PersonasAppConceptos.Services;
+
+using Xamarin.Forms;
+
+namespace PersonasAppConceptos.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        /// <summary>
+        /// Get the azure service instance
+        /// </summary>
+        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+        /// <summary>
+        /// Private backing field to hold the title
+        /// </summary>
+        string title = string.Empty;
+        /// <summary>
+        /// Public property to set and get the title of the item
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
+
